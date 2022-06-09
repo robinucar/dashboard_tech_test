11f9b4eb64865b2d7fdc3b3a73a436de3b9f7d96
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-06-10 00:43:03 +0100

    removed hard coding currency

--- client/src/App.js
+++ client/src/App.js
@@ -7,11 +7,12 @@ import "./App.css";
 function App() {
   const [data, setData] = useState([]);
   const [query, setQuery] = useState("");
+  
   useEffect(() => {
     // connect data
     const fetchData = async () => {
       const res = await axios.get(
-        "https://ldt-tech-test.herokuapp.com/api/startlistentries"
+        "http://localhost:5000/api/startlistentries"
       );
       setData(res.data);
     };
@@ -24,50 +25,23 @@ function App() {
       (data) => data.organiserTitle.toLowerCase().indexOf(query) > -1
     );
   };
-  // TOTAL CONFIRMED TICKETS SALES
-  const confirmedTicketsSum = data.reduce((total, item) => {
-    if (item.status === "CONFIRMED") {
-      total += item.ticketPrice.value;
-    }
-    return total;
-  }, 0);
-  // TOTAL PENDING TICKETS SALES
-  const pendingTicketsSum = data.reduce((total, item) => {
-    if (item.status === "PENDING") {
-      total += item.ticketPrice.value;
-    }
-    return total;
-  }, 0);
-
-  // TOTAL REFUNDED TICKETS SALES
-  const refundedTicketsSum = data.reduce((total, item) => {
-    if (item.status === "REFUNDED") {
-      total += item.ticketPrice.value;
-    }
-    return total;
-  }, 0);
-  //GREAT RUN TOTAL
-  const greatRunTotal = data.reduce((total, item) => {
-    if (item.organiserId === 26391) {
-      total += item.ticketPrice.value;
-    }
-    return total;
-  }, 0);
-  // Limelight Sports Club Total
-  const sportsClubTotal = data.reduce((total, item) => {
-    if (item.organiserId === 154979) {
-      total += item.ticketPrice.value;
-    }
-    return total;
-  }, 0);
-
-  // Run Through Total
-  const runThroughTotal = data.reduce((total, item) => {
-    if (item.organiserId === 69173) {
-      total += item.ticketPrice.value;
-    }
-    return total;
-  }, 0);
+  const generateStatusTicketsSum = (items, type) => {
+    return items.reduce((total, item) => {
+      if (item.status === type) {
+        total += item.ticketPrice.value
+      }
+      return total ;
+    }, 0) + " " + data[0].ticketPrice.currencyCode;
+  };
+  
+  const generateOrganiserTicketsSum = (items, id) => {
+    return items.reduce((total, item) => {
+      if (item.organiserId === id) {
+        total += item.ticketPrice.value;
+      }
+      return total;
+    }, 0) + " " + data[0].ticketPrice.currencyCode;;
+  };
   return (
     <div className="app">
       <input
@@ -77,22 +51,23 @@ function App() {
       />
       <DataTable data={search(data)} />
       <div className="tickets-data">
-        <h2>Total Confirmed Tickets Sales: {confirmedTicketsSum} GBP</h2>
-        <h2>Total Pending Tickets Sales: {pendingTicketsSum} GBP</h2>
-        <h2>Total Refunded Tickets Sales: {refundedTicketsSum} GBP</h2>
+        <h2>Total Confirmed Tickets Sales: {generateStatusTicketsSum(data, "CONFIRMED")} </h2>
+        <h2>Total Pending Tickets Sales: {generateStatusTicketsSum(data, "PENDING")} </h2>
+        <h2>Total Refunded Tickets Sales: {generateStatusTicketsSum(data, "REFUNDED")} </h2>
       </div>
 
       <div className="organisers">
         <h1>Organisers ticket sales</h1>
         <h2>
-          Total for Great Run Confirmed Tickets Sales: {greatRunTotal} GBP
+          Total for Great Run Confirmed Tickets Sales: {generateOrganiserTicketsSum(data, 26391)}
+          
         </h2>
         <h2>
           Total for Limelight Sports Club Confirmed Tickets Sales:{" "}
-          {sportsClubTotal} GBP
+          {generateOrganiserTicketsSum(data, 154979)} 
         </h2>
         <h2>
-          Total for Run Through Confirmed Tickets Sales: {runThroughTotal} GBP
+          Total for Run Through Confirmed Tickets Sales: {generateOrganiserTicketsSum(data, 69173)}
         </h2>
       </div>
     </div>
